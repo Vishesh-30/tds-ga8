1e16b622f77f0515477284dd38d73e5990154dfc
Vishesh Phutela <72484173+Vishesh-30@users.noreply.github.com>
2022-11-28 18:20:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
 
+st.header('Enter Values')
+def user_input_features():
+    First = st.number_input('Enter Second No.',min_value=0,max_value=2000000,step = 1)
+    Second = st.number_input('Enter Second No.',min_value=0,max_value=1000000,step = 1)
+    Third = st.number_input('Enter Third No.',min_value=0,max_value=900000,step = 1)
+    Numbers = [First, Second, Third]
+    return Numbers
+
+list = user_input_features()
+min = min(list)
+st.write(list)
+st.write('Minimum of three no is ')
+st.write(min)
